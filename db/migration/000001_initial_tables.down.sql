4ebed0c1f51042a5ffc5fc9e89915739b1769df0
Itamar-Ichaves <itmchaves@gmail.com>
2023-07-26 07:22:22 -0300

    prime commit sqlc

---
+++ db/migration/000001_initial_tables.down.sql
@@ -0,0 +1,18 @@
+CREATE TABLE "users" (
+    "id" serial PRIMARY KEY NOT NULL,
+    "username" varchar NOT NULL,
+    "password" varchar NOT NULL,
+    "email" varchar UNIQUE NOT NULL,
+    "created_at" timestamptz NOT NULL DEFAULT (now())
+
+)
+
+CREATE TABLE "categories" (
+    "id" serial PRIMARY KEY NOT NULL,
+    "user_id" int NOT NULL,
+    "title" varchar NOT NULL,
+    "type" varchar NOT NULL,
+    "description" varchar NOT NULL,
+    "created_at" timestamptz NOT NULL DEFAULT (now())
+
+)
