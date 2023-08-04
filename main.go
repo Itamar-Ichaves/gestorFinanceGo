6ad1ec8687ffcfed37209b88d41921eae4e07aa3
Itamar-Ichaves <itmchaves@gmail.com>
2023-08-04 10:56:23 -0300

    commit refresh

--- main.go
+++ main.go
@@ -3,19 +3,23 @@ package main
 import (
 	"database/sql"
 	"log"
+	"os"
 
-	api "github.com/Itamar-Ichaves/gestorFinanceGo/Api"
+	api "github.com/Itamar-Ichaves/gestorFinanceGo/api"
 	db "github.com/Itamar-Ichaves/gestorFinanceGo/db/sqlc"
+	"github.com/joho/godotenv"
 	_ "github.com/lib/pq"
 )
 
-const (
-	dbDriver     = "postgres"
-	dbSource     = "postgresql://postgres:postgres@localhost:5432/gestorfinancego?sslmode=disable"
-	serverAdress = "0.0.0.0:8000"
-)
-
 func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	dbDriver := os.Getenv("DB_DRIVER")
+	dbSource := os.Getenv("DB_SOURCE")
+	serverAddress := os.Getenv("SERVER_ADDRESS")
 	conn, err := sql.Open(dbDriver, dbSource)
 	if err != nil {
 		log.Fatal("cannot connect to db: ", err)
@@ -24,7 +28,7 @@ func main() {
 	store := db.NewStore(conn)
 	server := api.NewServer(store)
 
-	err = server.Start(serverAdress)
+	err = server.Start(serverAddress)
 	if err != nil {
 		log.Fatal("cannot start api: ", err)
 	}
